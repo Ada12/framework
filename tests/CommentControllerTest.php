e594fa2a6d65804ee0a8d2144ca3cace000bea7c
yangchen <ada_8463@163.com>
2015-04-25 15:19:07 +0800

    test passed

--- tests/CommentControllerTest.php
+++ tests/CommentControllerTest.php
@@ -10,14 +10,14 @@ class CommentControllerTest extends TestCase
     public function testStore()
     {
         Session::start();
-        $response = $this->call('POST', '/question/104/comment',[
+        $response = $this->call('POST', '/question/1/comment',[
             'content' => '第一个评论',
-            'user_id' => '3',
+            'user_id' => '1',
             '_token' => csrf_token(),
         ]);
         $body = json_decode($response->getContent(), true);
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals('第一个评论', $body['content']);
-        $this->assertEquals('3', $body['user_id']);
+        $this->assertEquals('1', $body['user_id']);
     }
 }
